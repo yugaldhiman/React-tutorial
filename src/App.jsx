3a01be614cbf3b7514f553958fb0f216abd541c4
yugaldhiman <yugaldhiman14@gmail.com>
2024-10-22 21:47:25 +0530

    solve this bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import OnePageAplication from './component/Class -14/OnePageAplication.jsx'
+import OnePageAplication from './component/Class-14/OnePageAplication.jsx'
 
 const App = () => {
   return (
