b6b86de142eca6970c5e896a89f3e3acaccd7000
yugaldhiman <yugaldhiman14@gmail.com>
2024-10-27 22:54:05 +0530

    consumer

--- src/component/Class-15/multiComp/Gcomp.jsx
+++ src/component/Class-15/multiComp/Gcomp.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { UserContext } from '../../../App';
 import Hcomp from './Hcomp'
 
 const Gcomp = () => {
   return (
-    <>
+    <div>
+      <UserContext.Consumer>
+        {
+          user => <h1>{user}</h1>
+        }
+      </UserContext.Consumer>
       <Hcomp />
-    </>
+    </div>
   )
 }
 
