b68ad52becf9f3321f7d5a06fe64828ef8ce3b3c
yugaldhiman <yugaldhiman14@gmail.com>
2024-11-07 12:31:48 +0530

    solve error

--- src/component/Class-16/Reduce.jsx
+++ src/component/Class-16/Reduce.jsx
@@ -1,17 +1,14 @@
 import React from 'react'
 
 const Reduce = () => {
-    const array = [0,1,2,3,4,5];
+    const array = [0, 1, 2, 3, 4, 5];
     const sumValue = array.reduce(
-        (fristValue,secondValue)=>fristValue+secondValue
+        (fristValue, secondValue) => fristValue + secondValue
     )
     console.log(sumValue);
-    
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <></>
+    )
 }
 
 export default Reduce
